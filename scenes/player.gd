c3fc1ee5fe12e58166549e591074f1343918637a
Josep Manclús <josep97mg@gmail.com>
2023-08-24 14:27:36 +0200

    heroine animations + movement

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-@export var speed: int = 35
+@export var speed: int = 45
+@onready var animations = $AnimationPlayer
 
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -9,3 +10,15 @@ func handleInput():
 func _physics_process(delta):
 	handleInput()
 	move_and_slide()
+	updateAnimation()
+
+func updateAnimation(): 
+	if velocity.length() == 0:
+		animations.play("iddle")
+	else: 
+		var animationDirection = "down"
+		if velocity.x < 0: animationDirection = "left"
+		elif velocity.x > 0: animationDirection = "right"
+		elif velocity.y < 0: animationDirection = "up"
+		
+		animations.play("walk_" + animationDirection)
