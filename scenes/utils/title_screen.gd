c2929f1a4cdd932a83ca331875688df418b0303b
Josep Manclús <josep97mg@gmail.com>
2023-09-14 21:37:11 +0200

    title screen implementation

--- scenes/utils/title_screen.gd
+++ scenes/utils/title_screen.gd
@@ -5,16 +5,74 @@ extends Control
 @onready var flash = $ColorRect/FlashPlayer
 @onready var buttons = $Buttons 
 @onready var transition = $Transitions
-@onready var exit_button_sound = $Buttons/ExitButton/ExitButtonPressed
-@onready var play_button_sound = $Buttons/PlayButton/PlayButtonPressed
+@onready var exit_button_sound = $ExitButtonPressed
+@onready var play_button_sound = $PlayButtonPressed
+@onready var change_button_sound = $ChangeButton
+@onready var house_interior_scene = load("res://scenes/maps/heroine_house_interior.tscn")
+@onready var selector_one = $Buttons/StartButton/Selector
+@onready var selector_two = $Buttons/ExitButton/Selector
+@onready var start_button = $Buttons/StartButton/Start
+@onready var exit_button = $Buttons/ExitButton/Exit
+
+@onready var not_intro :bool = false 
+var current_selection = 0 
+var selected_button
+var tween
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	set_selected_button(0)
 	transition.enter_screen()
-	buttons.self_modulate = Color(1, 1, 1, 0)
+	start_button.self_modulate = Color(1, 1, 1, 0)
+	exit_button.self_modulate = Color(1, 1, 1, 0)
+	selector_one.self_modulate = Color(1, 1, 1, 0)
+	selector_two.self_modulate = Color(1, 1, 1, 0)
 	var tween = create_tween()
 	tween.tween_property(heart, "position", Vector2(120,67), 2)
 	tween.connect("finished", _animate_heart_loop)
+	
+	
+func _process(delta):
+	if Input.is_action_just_pressed("ui_down") and current_selection < 1:
+		current_selection += 1
+		set_selected_button(current_selection)
+	elif Input.is_action_just_pressed("ui_up") and current_selection > 0:
+		current_selection -= 1
+		set_selected_button(current_selection)
+	elif Input.is_action_just_pressed("attack_button") or Input.is_action_just_pressed("interact_button"):
+		press_button(current_selection)
+	
+func press_button(_current_selection):
+	match _current_selection:
+		0: # start button
+			play_button_sound.play()
+		1: # exit button
+			exit_button_sound.play()
+			
+func _play_button_animation():
+	if tween != null: 
+		tween.stop()
+	
+	tween = create_tween()
+	tween.set_ease(Tween.EASE_IN)
+	tween.set_trans(Tween.TRANS_EXPO)
+	tween.tween_property(selected_button, "theme_override_font_sizes/font_size", 8, 0.4)
+	tween.tween_property(selected_button, "theme_override_font_sizes/font_size", 10, 0.4)
+	tween.set_loops()
+
+func set_selected_button(_current_selection):
+	selector_one.text = ""
+	selector_two.text = ""
+	if not_intro:
+		$ChangeButton.play()
+	match _current_selection:
+		0:  # start 
+			selected_button = selector_one
+			selector_one.text = "*"
+		1:  # exit
+			selected_button = selector_two
+			selector_two.text = "*"
+	_play_button_animation()
 
 func _animate_heart_loop():
 	music.play()
@@ -34,17 +92,18 @@ func _animate_button_letters():
 
 func _play_flash():
 	var tween = create_tween()
+	tween.set_parallel(true)
 	tween.set_ease(Tween.EASE_OUT)
 	tween.set_trans(Tween.TRANS_BOUNCE)
-	tween.tween_property(buttons, "self_modulate", Color(1, 1, 1, 1), 1.5)
+	tween.tween_property(start_button, "self_modulate", Color(1, 1, 1, 1), 1.5)
+	tween.tween_property(exit_button, "self_modulate", Color(1, 1, 1, 1), 1.5)
+	tween.tween_property(selector_one, "self_modulate", Color(1, 1, 1, 1), 1.5)
+	tween.tween_property(selector_two, "self_modulate", Color(1, 1, 1, 1), 1.5)
 	flash.play("flash")
-
-func _on_flash_player_animation_finished(anim_name):
-	if anim_name == "flash": 
-		pass
-
-func _on_exit_button_pressed():
-	exit_button_sound.play()
+	not_intro = true
 
 func _on_exit_button_pressed_finished():
 	get_tree().quit()
+
+func _on_play_button_pressed_finished():
+	transition.exit_screen(house_interior_scene)
